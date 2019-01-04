606261f78e595b840fdbc9937e6f5a0464a9ce32
Leo Torres <leohtor@gmail.com>
2019-01-03 19:23:52 -0800

    init commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+// import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <div>
+      <h1>JS Calculator</h1>
+      <div id="equals">
+        <button type="button">=</button>
+      </div>
+    </div>
+    
+    
+  </Layout>
+)
+
+export default IndexPage
+
