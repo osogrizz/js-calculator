9145478126de72e2552e24a2990de4f8bc4edfb1
Leo Torres <leohtor@gmail.com>
2019-01-03 20:02:30 -0800

    Added digits and operator buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,24 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div>
       <h1>JS Calculator</h1>
+      <div>
+        <buttton id="zero"></buttton>
+        <button id="one"></button>
+        <button id="two"></button>
+        <button id="three"></button>
+        <button id="four"></button>
+        <button id="five"></button>
+        <button id="six"></button>
+        <button id="seven"></button>
+        <button id="eight"></button>
+        <button id="nine"></button>
+      </div>
+      <div>
+        <button id="add"></button>
+        <button id="subtract"></button>
+        <button id="multiply"></button>
+        <button id="divide"></button>
+      </div>
       <div id="equals">
         <button type="button">=</button>
       </div>
