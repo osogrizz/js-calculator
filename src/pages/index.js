095b8e5beb3b652c4a474a54314a2722699b201d
Leo Torres <leohtor@gmail.com>
2019-01-03 20:26:53 -0800

    Converted to a class component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,50 @@ import Layout from '../components/layout'
 // import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+//     <div>
+//       <h1>JS Calculator</h1>
+//       <div id="display">
+//       </div>
+//       <div>
+//         <button id="clear">AC</button>
+//         <button id="zero">0</button>
+//         <button id="one">1</button>
+//         <button id="two">2</button>
+//         <button id="three">3</button>
+//         <button id="four">4</button>
+//         <button id="five">5</button>
+//         <button id="six">6</button>
+//         <button id="seven">7</button>
+//         <button id="eight">8</button>
+//         <button id="nine">9</button>
+//         <button id="decimal">.</button>
+//       </div>
+//       <div>
+//         <button id="add">+</button>
+//         <button id="subtract">-</button>
+//         <button id="multiply">x</button>
+//         <button id="divide">%</button>
+//       </div>
+//       <div id="equals">
+//         <button type="button">=</button>
+//       </div>
+//     </div>
+    
+    
+//   </Layout>
+// )
+
+// export default IndexPage
+
+import React, { Component } from 'react'
+
+export default class IndexPage extends Component {
+  render() {
+    return (
+      <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div>
       <h1>JS Calculator</h1>
@@ -38,7 +80,6 @@ const IndexPage = () => (
     
     
   </Layout>
-)
-
-export default IndexPage
-
+    )
+  }
+}
